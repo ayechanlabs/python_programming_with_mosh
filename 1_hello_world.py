8ac311ec84364f7b3a514ab9865e83f0c295b760
alexnyeinlabs <acn.alexchan@gmail.com>
2024-09-17 21:19:35 +0700

    first-commit

---
+++ 1_hello_world.py
@@ -0,0 +1,15 @@
+print("I'm Aye Chan Nyein")
+
+print('o----')
+print(' ||||')
+
+print('*' * 10)  # this piece of code is called expression.
+
+"""
+Python code executed line by line from the top.
+
+In python, when we use quotation such as single,double we're defining a string.
+A string in programming term is a series of characters.
+
+An expression is a code that produces a value.
+"""
