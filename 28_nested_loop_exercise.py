d000ef7bb2653029daa693d1e88a7004b004cc8e
alexnyeinlabs <acn.alexchan@gmail.com>
2024-09-26 15:17:37 +0700

    nested_loop

---
+++ 28_nested_loop_exercise.py
@@ -0,0 +1,22 @@
+# Version 1
+for x in range(2):
+    print('x' * 5)
+    for y in range(0, 3, 2):
+        if x == 0:
+            print('x' * 2)
+            break
+        else:
+            print('x' * 2)
+
+# Version 2
+numbers = [5, 2, 5, 2, 2]
+
+# for count in numbers:
+#     # print(count) # it will print each number from list
+#     print('x' * count)
+
+for counter in numbers:
+    output = ''
+    for count in range(counter):
+        output += 'x'
+    print(output)
